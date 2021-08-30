efe58fcdd2068d118e478c409bde30f195a38a50
zeipherK0de168 <aortizcristo@gmail.com>
2021-08-30 15:15:22 -0600

    first commit

---
+++ useCounter.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const useCounter = (initialState = 10) => {
+  const [state, setState] = useState(initialState); //1 0
+
+  const increment = (factor = 1) => {
+    setState(state + factor);
+  };
+
+  const decrement = (factor = 1) => {
+    setState(state - factor);
+  };
+
+  const reset = () => {
+    setState(initialState);
+  }
+
+  return {
+    state,
+    increment,
+    decrement,
+    reset,
+  }
+};
+
+export default useCounter;
